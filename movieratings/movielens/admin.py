17f5c9ea162cdab84ceb7af8e5e65a8d10ce529f
Ryan Ransom <Jhonais@Ryans-MacBook-Pro.local>
2015-10-22 09:11:45 -0700

    Django homework 10-21-15

---
+++ movieratings/movielens/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from .models import Movies, Rantings, Genre, Links, Tags
+
+admin.site.register(Movies)
+admin.site.register(Rantings)
+admin.site.register(Genre)
+admin.site.register(Links)
+admin.site.register(Tags)
