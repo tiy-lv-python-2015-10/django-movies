f22ea0bb220f399f96e14ce57763d3d71c896998
jp <jp.park@me.com>
2015-11-02 18:04:43 -0800

    updated models

--- movierater/movierater_rates/models.py
+++ movierater/movierater_rates/models.py
@@ -5,32 +5,37 @@ class Links(models.Model):
     movieid = models.IntegerField()
     imdbid = models.IntegerField()
     tmdbid = models.IntegerField()
-    Add_date = models.DateTimeField(auto_now_add=True)
+    add_date = models.DateTimeField(auto_now_add=True)
     modified_date = models.DateTimeField(auto_now=True)
+
+
+class Genres(models.Model):
+    genres = models.CharField(max_length=50)
+    genreid = models.IntegerField(primary_key=True)
 
 
 class Movies(models.Model):
     movieid = models.IntegerField()
     title = models.CharField(max_length=100)
-    genres = models.CharField(max_length=100)
-    Add_date = models.DateTimeField()
-    modified_date = models.DateTimeField()
+    genreid = models.ForeignKey(Genres)
+    add_date = models.DateTimeField(auto_now_add=True)
+    modified_date = models.DateTimeField(auto_now=True)
 
 
 class Ratings(models.Model):
     ratingid = models.IntegerField()
     userid = models.IntegerField()
-    movieid = models.IntegerField()
+    movieid = models.ForeignKey(Movies)
     rating = models.DecimalField(decimal_places=2, max_digits=2)
-    timestamp = models.DateTimeField()
-    Add_date = models.DateTimeField()
-    modified_date = models.DateTimeField()
+    timestamp = models.IntegerField()
+    add_date = models.DateTimeField(auto_now_add=True)
+    modified_date = models.DateTimeField(auto_now=True)
 
 
 class Tags(models.Model):
     userid = models.IntegerField()
     movieid = models.IntegerField()
     tag = models.CharField(max_length=100)
-    timestamp = models.DateTimeField()
-    Add_date = models.DateTimeField()
-    modified_date = models.DateTimeField()
+    timestamp = models.IntegerField()
+    add_date = models.DateTimeField(auto_now_add=True)
+    modified_date = models.DateTimeField(auto_now=True)
